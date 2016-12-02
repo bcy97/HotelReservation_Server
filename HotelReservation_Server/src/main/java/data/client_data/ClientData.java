83ce31b7dc067e79979ff3c3eb9119ae2ca56079
bcy97 <609233559@qq.com>
2016-12-02 11:31:34 +0800

    完成ClientData

--- HotelReservation_Server/src/main/java/data/client_data/ClientData.java
+++ HotelReservation_Server/src/main/java/data/client_data/ClientData.java
@@ -1,22 +1,95 @@
 package data.client_data;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import data.database.DBConnection;
 import dataDao.ClientDao;
 import po.ClientPO;
 
 public class ClientData implements ClientDao{
 
 	public boolean addClient(ClientPO po) {
-		// TODO Auto-generated method stub
+		Connection conn;
+		Statement statement;
+		String sql = "insert into client values('" 
+						+po.getUserID() + "','" + po.getPhoneNumber() +"','"+po.getTrueName()+"','"+po.getIdentityID()+"','"+po.getHeadImagePath()
+						+"','"+po.getVipType()+"','"+po.getVipLevel()+"','"+po.getVipInfo()+"')";		
+		try {
+			conn  = DBConnection.getConnection();
+			statement = conn.createStatement();
+			statement.executeUpdate(sql);
+			statement.close();
+			conn.close();
+			
+			return true;
+			
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 		return false;
 	}
 
 	public ClientPO getClientInfo(String clientID) {
-		// TODO Auto-generated method stub
+		Connection conn;
+		Statement statement;
+		ClientPO clientPO;
+		String sql = "select * from account where userID='"+clientID+"'";
+		
+		try {
+			conn = DBConnection.getConnection();
+			statement = conn.createStatement();
+
+			ResultSet rs = statement.executeQuery(sql);
+			if (rs.next()) {
+				clientPO = new ClientPO(null, null, null, null, null, 0, 0, null);
+				clientPO.setUserID(rs.getString("userID"));
+				clientPO.setPhoneNumber(rs.getString("phoneNumber"));
+				clientPO.setTrueName(rs.getString("trueName"));
+				clientPO.setIdentityID(rs.getString("identityID"));
+				clientPO.setHeadImagePath(rs.getString("headImagePath"));
+				clientPO.setVipType(rs.getInt("vipType"));
+				clientPO.setVipLevel(rs.getInt("vipLevel"));
+				clientPO.setVipInfo(rs.getString("vioInfo"));
+				
+				statement.close();
+				conn.close();
+				
+				return clientPO;
+			}
+			
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 		return null;
 	}
 
 	public boolean updateClientInfo(ClientPO clientPO) {
-		// TODO Auto-generated method stub
+		Connection conn;
+		Statement statement;
+		String sql = "update account set phoneNumber='"+clientPO.getPhoneNumber()
+					+"',trueName='"+clientPO.getTrueName()
+					+"',identityID='"+clientPO.getIdentityID()
+					+"',headImagePath='"+clientPO.getHeadImagePath()
+					+"',vipType="+clientPO.getVipType()
+					+",vipLevel="+clientPO.getVipLevel()
+					+",vipInfo='"+clientPO.getVipInfo()
+					+"' where AccountID='"+clientPO.getUserID()+"'";
+		try {
+			conn  = DBConnection.getConnection();
+			statement = conn.createStatement();
+			statement.executeUpdate(sql);
+			
+			statement.close();
+			conn.close();
+			
+			return true;
+			
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 		return false;
 	}
 
