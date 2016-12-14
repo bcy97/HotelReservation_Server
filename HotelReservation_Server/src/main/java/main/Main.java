8a5744d4ba4c41a63c2f5c303b83097c062f31cf
bcy97 <609233559@qq.com>
2016-12-14 11:44:35 +0800

    建一个启动界面

--- HotelReservation_Server/src/main/java/main/Main.java
+++ HotelReservation_Server/src/main/java/main/Main.java
@@ -1,11 +1,36 @@
 package main;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import rmi.RemoteHelper;
 
 public class Main {
 
 	public static void main(String[] args) {
-		new RemoteHelper();
+		Main main = new Main();
+		main.createFrame();
 	}
 	
+	public void createFrame() {
+		JFrame mainFrame = new JFrame("HotelServer");
+		JButton button = new JButton("start");
+		button.addActionListener(new ButtonActionListener());
+		mainFrame.getContentPane().add(button);
+		
+		mainFrame.setVisible(true);
+		mainFrame.setSize(300, 200);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	private class ButtonActionListener implements ActionListener{
+
+		public void actionPerformed(ActionEvent e) {
+			new RemoteHelper();
+		}
+		
+	}
 }
